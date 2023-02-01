a78992d6cba617334a57aa77a78e4ab2d8327485
Brekke Green <brekke.andrew.green@gmail.com>
2023-02-01 10:54:29 -0500

    Change color from blue to green

--- todo_reminder_app/lib/main.dart
+++ todo_reminder_app/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         // This is the theme of your application.
         //
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.green,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
